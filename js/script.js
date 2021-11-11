51c7e7d5966e087a2c8ca516192d809cf303c38a
Olga <example@gmail.com>
2021-11-11 15:57:26 +0300

    add menu cards

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
+class MenuCard {
+    constructor (src, alt, title, description, price, parentSelector) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 27;
+        this.changeToUah();
+
+        console.log('construct', parentSelector, this.parent);
+    }
+
+
+    changeToUah () {
+        this.price = this.price * this.transfer;
+    }
+
+    render () {
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.description}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+        `;
+        this.parent.append(element);
+    }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
 
+
+    
     // Tabs
     
 	let tabs = document.querySelectorAll('.tabheader__item'),
@@ -38,10 +77,10 @@ tabsParent.addEventListener('click', function(event) {
         });
     }
 });
-});
+
 
     //Timer
-    const deadline = '2021-12-26';
+    const deadline = '2021-12-27';
 
     //Задача получить разницу между датами
 
@@ -51,8 +90,6 @@ tabsParent.addEventListener('click', function(event) {
             hours = Math.floor((t / (1000 * 60 * 60 ) % 24)),
             minutes = Math.floor((t / 1000 / 60) % 60),
             seconds = Math.floor((t / 1000) % 60); 
-
-            console.log(t);
 
         return {
             'total': t,
@@ -148,4 +185,38 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
     }
 
     window.addEventListener('scroll', showModalByScroll);
+    });
+
+
+
+
+    //Экземпляры класса с карточками меню
+    
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        20,
+        ".menu .container"
+    ).render();
+
 });
